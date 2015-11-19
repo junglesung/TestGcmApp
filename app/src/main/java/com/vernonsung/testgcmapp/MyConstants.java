86224b2cefd5972ecd499481292567b0a8bde7fd
Vernon Sung <vernonsung@itri.org.tw>
2015-11-19 18:10:37 +0800

    Implement getToken(). TODO: implement deleteToken()

--- app/src/main/java/com/vernonsung/testgcmapp/MyConstants.java
+++ app/src/main/java/com/vernonsung/testgcmapp/MyConstants.java
@@ -7,4 +7,13 @@ public class MyConstants {
     public static final String UNREGISTRATION_COMPLETE = "unregistrationComplete";
     public static final String ACTION_GET_TOKEN = "actionGetToken";
     public static final String ACTION_DELETE_TOKEN = "actionDeleteToken";
+
+    // URL
+    public static final String APP_SERVER_URL_BASE = "https://testgcmserver-1120.appspot.com/api/0.1";
+    public static final String ECHO_URL = APP_SERVER_URL_BASE + "/tokens";
+    public static final String USER_REGISTRATION_URL = APP_SERVER_URL_BASE + "/users";
+
+    // Values
+    public static final int URL_CONNECTION_READ_TIMEOUT = 10000;  // milliseconds
+    public static final int URL_CONNECTION_CONNECT_TIMEOUT = 15000;  // milliseconds
 }
--- app/src/main/java/com/vernonsung/testgcmapp/RegistrationIntentService.java
+++ app/src/main/java/com/vernonsung/testgcmapp/RegistrationIntentService.java
@@ -1,14 +1,27 @@
 package com.vernonsung.testgcmapp;
 
 import android.app.IntentService;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.preference.PreferenceManager;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 import com.google.android.gms.iid.InstanceID;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 /**
  * An {@link IntentService} subclass for handling asynchronous task requests in
@@ -31,15 +44,19 @@ public class RegistrationIntentService extends IntentService {
         switch (action) {
             case MyConstants.ACTION_GET_TOKEN:
                 getToken();
+                break;
             case MyConstants.ACTION_DELETE_TOKEN:
                 deleteToken();
+                break;
             default:
                 Log.d(LOG_TAG, "Registration service received an unrecognized action");
         }
     }
 
     private void getToken() {
+        // Get current token from shared preferences
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String oldToken = sharedPreferences.getString(MyConstants.REGISTRATION_TOKEN, "");
 
         try {
             // [START register_for_gcm]
@@ -49,12 +66,18 @@ public class RegistrationIntentService extends IntentService {
             InstanceID instanceID = InstanceID.getInstance(this);
 
             // gcm_defaultSenderId comes from google-services.json automatically through Android Studio compiler
-            String token = instanceID.getToken(getString(R.string.gcm_defaultSenderId),
+            String newToken = instanceID.getToken(getString(R.string.gcm_defaultSenderId),
                     GoogleCloudMessaging.INSTANCE_ID_SCOPE, null);
             // [END get_token]
-            Log.i(LOG_TAG, "GCM Registration Token: " + token);
+            Log.i(LOG_TAG, "GCM Registration Token: " + newToken);
 
-
+            // Update new token to server
+            UserRegistration registration = new UserRegistration(oldToken, instanceID.getId(), newToken, "");
+            if (sendTokenToServer(registration) != 0) {
+                throw new Exception("Send token to server failed");
+            }
+            // Vernon debug
+            Log.d(LOG_TAG, "Successfully send registration token " + registration.toString());
             // Now it can receive messages
 
             // You should store a boolean that indicates whether the generated token has been
@@ -62,10 +85,10 @@ public class RegistrationIntentService extends IntentService {
             // otherwise your server should have already received the token.
             sharedPreferences.edit().putBoolean(MyConstants.SENT_TOKEN_TO_SERVER, true).apply();
             // Store token
-            sharedPreferences.edit().putString(MyConstants.REGISTRATION_TOKEN, token).apply();
+            sharedPreferences.edit().putString(MyConstants.REGISTRATION_TOKEN, newToken).apply();
             // [END register_for_gcm]
         } catch (Exception e) {
-            Log.d(LOG_TAG, "Failed to complete token refresh", e);
+            Log.d(LOG_TAG, "Failed to complete token refresh because " + e.getMessage());
             // If an exception happens while fetching the new token or updating our registration data
             // on a third-party server, this ensures that we'll attempt the update at a later time.
             sharedPreferences.edit().putBoolean(MyConstants.SENT_TOKEN_TO_SERVER, false).apply();
@@ -109,5 +132,85 @@ public class RegistrationIntentService extends IntentService {
         // Notify UI that registration has completed, so the progress indicator can be hidden.
         Intent unregistrationComplete = new Intent(MyConstants.UNREGISTRATION_COMPLETE);
         LocalBroadcastManager.getInstance(this).sendBroadcast(unregistrationComplete);
+    }
+
+    // Send new registration token to APP server so that the server can send message to this device through Google Cloud Messaging
+    private int sendTokenToServer(UserRegistration registration) {
+        // Check network connection ability and then access Google Cloud Storage
+        ConnectivityManager connMgr = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo == null || !networkInfo.isConnected()) {
+            Toast.makeText(this, getString(R.string.no_network_connection_available), Toast.LENGTH_LONG).show();
+            return -1;
+        }
+
+        // Send
+        URL url;
+        HttpsURLConnection urlConnection = null;
+        int size;
+        int ret = 0;
+        byte[] data;
+        OutputStream out;
+        String userUrl;
+
+        try {
+            url = new URL(MyConstants.USER_REGISTRATION_URL + "/" + registration.getInstanceId());
+            urlConnection = (HttpsURLConnection) url.openConnection();
+
+            // Set HTTP method
+            urlConnection.setRequestMethod("PUT");
+
+            // Set content type
+            urlConnection.setRequestProperty("Content-Type", "application/json");
+
+            // To upload data to a web server, configure the connection for output using setDoOutput(true). It will use POST if setDoOutput(true) has been called.
+            urlConnection.setDoOutput(true);
+
+            // Convert item to JSON string
+            data = registration.toJSONObject().toString().getBytes();
+//            data = new Gson().toJson(registration).getBytes();
+
+            // For best performance, you should call either setFixedLengthStreamingMode(int) when the body length is known in advance, or setChunkedStreamingMode(int) when it is not. Otherwise HttpURLConnection will be forced to buffer the complete request body in memory before it is transmitted, wasting (and possibly exhausting) heap and increasing latency.
+            size = data.length;
+            if (size > 0) {
+                urlConnection.setFixedLengthStreamingMode(size);
+            } else {
+                // Set default chunk size
+                urlConnection.setChunkedStreamingMode(0);
+            }
+
+            // Get the OutputStream of HTTP client
+            out = new BufferedOutputStream(urlConnection.getOutputStream());
+            // Copy from file to the HTTP client
+            out.write(data);
+            // Make sure to close streams, otherwise "unexpected end of stream" error will happen
+            out.close();
+
+            // Set timeout
+            urlConnection.setReadTimeout(MyConstants.URL_CONNECTION_READ_TIMEOUT);
+            urlConnection.setConnectTimeout(MyConstants.URL_CONNECTION_CONNECT_TIMEOUT);
+
+            // Send and get response
+            // getResponseCode() will automatically trigger connect()
+            int responseCode = urlConnection.getResponseCode();
+            String responseMsg = urlConnection.getResponseMessage();
+            Log.d(LOG_TAG, "Response " + responseCode + " " + responseMsg);
+            if (responseCode != HttpURLConnection.HTTP_NO_CONTENT) {
+                return -1;
+            }
+
+            // Get image URL
+            userUrl = urlConnection.getHeaderField("Location");
+            Log.d(LOG_TAG, "User URL " + userUrl);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            ret = -1;
+        } finally {
+            if (urlConnection != null)
+                urlConnection.disconnect();
+        }
+
+        return ret;
     }
 }
