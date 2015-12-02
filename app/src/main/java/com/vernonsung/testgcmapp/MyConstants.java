9423ebfa6720de88cdef142d6bc9d15ade98c899
Vernon Sung <vernonsung@itri.org.tw>
2015-12-02 14:37:31 +0800

    Finish testing joining and leaving a group. TODO: Implement sending group messages.

--- app/src/main/java/com/vernonsung/testgcmapp/MyConstants.java
+++ app/src/main/java/com/vernonsung/testgcmapp/MyConstants.java
@@ -21,6 +21,9 @@ public class MyConstants {
     public static final String TOPIC_MESSAGE_URL = APP_SERVER_URL_BASE + "/topic-messages";
     public static final String GROUP_URL = APP_SERVER_URL_BASE + "/groups";
 
+    // HTTP HEADER
+    public static final String HTTP_HEADER_INSTANCE_ID = "Instance-Id";
+
     // Values
     public static final int URL_CONNECTION_READ_TIMEOUT = 10000;  // milliseconds
     public static final int URL_CONNECTION_CONNECT_TIMEOUT = 15000;  // milliseconds
